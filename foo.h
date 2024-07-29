5b04d2630cfe71a11534c17ed27dc64e887c2a72
deven367 <masterdeven@gmail.com>
2024-07-28 22:45:59 -0400

    messing with structs

--- foo.h
+++ foo.h
@@ -3,3 +3,12 @@
 int fun(int a, int b){
     return a + b;
 }
+
+typedef struct  {
+    int a;
+    int b;
+} Fun;
+
+void print_fun(Fun x){
+    printf("Values in the struct are %d and %d\n", x.a, x.b);
+}
--- test.c
+++ test.c
@@ -4,5 +4,11 @@ int main(){
     printf("Hello World!\n");
     printf("%d\n", CONST);
     printf("Value of function fun is %d\n", fun(5, 10));
+
+
+    Fun f = { 5, 5};
+    // f.a = 5;
+    // f.b = 5;
+    print_fun(f);
     return 0;
 }
